cc503884e9728758081dfffedfdef27fc655a763
Flavio Alejandro Marin Cordero <cordero@informatik.tu-muenchen.de>
2012-04-28 14:20:44 +0200

    5 discretization functions in x

--- 01/MATERIAL/disc.c
+++ 01/MATERIAL/disc.c
@@ -85,7 +85,96 @@ double** add_mat(double** M, double** N, int nrl, int nrh, int ncl, int nch)
 }
 
 
+double** duvdy(double** U, double **V, double dy, int nrl, int nrh, int ncl, int nch)
+{
 
+    int i = 0;
+    int j = 0;
+
+    double firstOperand = 0;
+    double secondOperand = 0;
+    double alpha = 0;
+
+    double** result = NULL;
+
+    for(i = nrl; i< nrh; ++i)
+    {
+        for(j = ncl; j < nch; ++j)
+        {
+            firstOperand = ( ( 1- alpha ) / dy )*
+            ( ( V[i][j] + V[i+1][j] ) / 2 * ( U[i][j] + U[i][j+1] ) / 2
+            - ( V[i][j-1] + V[i+1][j-1] ) / 2 * ( U[i][j] + U[i][j-1] ) / 2);
+
+            secondOperand = ( alpha / dy )*
+            ( ( (abs(V[i][j] + V[i+1][j])/2) * ((U[i][j] - U[i][j+1])/2) )
+            -( (abs(V[i][j-1] + V[i+1][j-1])/2) * ((U[i][j-1] - U[i][j])/2) ));
+            result[i][j] = firstOperand + secondOperand;
+        }
+    }
+
+    return result;
+}
+
+
+double** d2udx2(double** U, double dx, int nrl, int nrh, int ncl, int nch)
+{
+    int i = 0;
+    int j = 0;
+
+    double** result = NULL;
+
+    for( i = nrl; i < nrh; ++i)
+    {
+        for( j = ncl; j < nch; ++j)
+        {
+            result[i][j] = (U[i-1][j]-2*U[i][j]+U[i+1][j]) / pow(dx,2);
+        }
+     }
+
+
+     return result;
+
+}
+
+
+
+double** d2udy2(double** U, double dy, int nrl, int nrh, int ncl, int nch)
+{
+    int i = 0;
+    int j = 0;
+
+    double** result = NULL;
+
+    for( i = nrl; i < nrh; ++i)
+    {
+        for( j = ncl; j < nch; ++j)
+        {
+            result[i][j] = ( U[i-1][j]-2*U[i][j]+U[i+1][j]) / pow( dy,2 );
+        }
+    }
+
+    return result;
+
+}
+
+double** dpdx(double** P, double dx, int nrl, int nrh, int ncl, int nch)
+{
+    int i = 0;
+    int j = 0;
+
+    double** result = NULL;
+
+    for( i = nrl; i < nrh; ++i)
+    {
+        for( j = ncl; j < nch; ++j)
+        {
+            result[i][j]=(P[i+1][j]-P[i][j])/dx;
+        }
+    }
+
+    return result;
+
+}
 
 
 
--- 01/MATERIAL/disc.h
+++ 01/MATERIAL/disc.h
@@ -5,11 +5,13 @@ double** du2dx(double** U, double dx, int nrl, int nrh, int ncl, int nch);
 double** duvdy(double** U, double **V, double dy, int nrl, int nrh, int ncl, int nch);
 double** d2udx2(double** U, double dx, int nrl, int nrh, int ncl, int nch); 
 double** d2udy2(double** U, double dy, int nrl, int nrh, int ncl, int nch); 
+double** dpdx(double** P, double dx, int nrl, int nrh, int ncl, int nch);
 
 double** dv2dy(double** V, double dy, int nrl, int nrh, int ncl, int nch);
 double** duvdx(double** U, double **V, double dx, int nrl, int nrh, int ncl, int nch);
 double** d2vdx2(double** V, double dx, int nrl, int nrh, int ncl, int nch); 
 double** d2vdy2(double** V, double dy, int nrl, int nrh, int ncl, int nch); 
+double** dpdy(double** P, double dy, int nrl, int nrh, int ncl, int nch);
 
 /*adds scalar to all elements of a matrix*/
 double** add_scalar(double** M, int nrl, int nrh, int ncl, int nch, double s);
