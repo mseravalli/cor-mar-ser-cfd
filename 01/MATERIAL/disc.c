4ec7b094c7564ed09a4bff5ff02d285b7831b3c0
marco.seravalli@gmail.com <marco.seravalli@gmail.com>
2012-04-28 20:06:49 +0200

    added all functions for calculating eq (1) and (2)

--- 01/MATERIAL/disc.c
+++ 01/MATERIAL/disc.c
@@ -1,7 +1,29 @@
 #include "disc.h"
 #include "helper.h"
 
-double** du2dx(double** U, double dx, int nrl, int nrh, int ncl, int nch)
+/******** PRIVATE DERIVATIVE FUNCTIONS START ********/
+/** TODO: all the methods actually do not initialise the result before using it!! **/
+
+double** dMds(double** M, double ds, int nrl, int nrh, int ncl, int nch)
+{
+    int i = 0;
+    int j = 0;
+
+    double** result = NULL;
+
+    for( i = nrl; i < nrh; ++i)
+    {
+        for( j = ncl; j < nch; ++j)
+        {
+            result[i][j]=(M[i+1][j]-M[i][j]) / ds;
+        }
+    }
+
+    return result;
+
+}
+
+double** dM2ds(double** M, double ds, int nrl, int nrh, int ncl, int nch)
 {
     int i = 0;
     int j = 0;
@@ -16,12 +38,12 @@ double** du2dx(double** U, double dx, int nrl, int nrh, int ncl, int nch)
     {
         for(j = ncl; j < nch; ++j)
         {
-            firstOperand = ( (1-alpha) / dx) *
-                ( pow((U[i][j] + U[i+1][j])/2, 2) -  pow((U[i][j] + U[i-1][j])/2, 2));
+            firstOperand = ( (1-alpha) / ds) *
+                ( pow((M[i][j] + M[i+1][j])/2, 2) -  pow((M[i][j] + M[i-1][j])/2, 2));
 
-            secondOperand = ( alpha / dx ) *
-                ( ( (abs(U[i][j] + U[i+1][j])/2) * ((U[i][j] - U[i+1][j])/2) )  
-                - ( (abs(U[i][j] + U[i-1][j])/2) * ((U[i-1][j] - U[i][j])/2) ) );
+            secondOperand = ( alpha / ds ) *
+                ( ( (abs(M[i][j] + M[i+1][j])/2) * ((M[i][j] - M[i+1][j])/2) )  
+                - ( (abs(M[i][j] + M[i-1][j])/2) * ((M[i-1][j] - M[i][j])/2) ) );
 
             result[i][j] = firstOperand + secondOperand;
 
@@ -29,12 +51,11 @@ double** du2dx(double** U, double dx, int nrl, int nrh, int ncl, int nch)
     }
 
    return result; 
-
 }
 
-double** duvdy(double** U, double **V, double dy, int nrl, int nrh, int ncl, int nch)
-{
 
+double** dMNds(double** M, double **N, double ds, int nrl, int nrh, int ncl, int nch)
+{
     int i = 0;
     int j = 0;
 
@@ -48,13 +69,13 @@ double** duvdy(double** U, double **V, double dy, int nrl, int nrh, int ncl, int
     {
         for(j = ncl; j < nch; ++j)
         {
-            firstOperand = ( ( 1- alpha ) / dy )*
-            ( ( V[i][j] + V[i+1][j] ) / 2 * ( U[i][j] + U[i][j+1] ) / 2
-            - ( V[i][j-1] + V[i+1][j-1] ) / 2 * ( U[i][j] + U[i][j-1] ) / 2);
+            firstOperand = ( ( 1- alpha ) / ds )*
+            ( ( N[i][j] + N[i+1][j] ) / 2 * ( M[i][j] + M[i][j+1] ) / 2
+            - ( N[i][j-1] + N[i+1][j-1] ) / 2 * ( M[i][j] + M[i][j-1] ) / 2);
 
-            secondOperand = ( alpha / dy )*
-            ( ( (abs(V[i][j] + V[i+1][j])/2) * ((U[i][j] - U[i][j+1])/2) )
-            -( (abs(V[i][j-1] + V[i+1][j-1])/2) * ((U[i][j-1] - U[i][j])/2) ));
+            secondOperand = ( alpha / ds )*
+            ( ( (abs(N[i][j] + N[i+1][j])/2) * ((M[i][j] - M[i][j+1])/2) )
+            - ( (abs(N[i][j-1] + N[i+1][j-1])/2) * ((M[i][j-1] - M[i][j])/2) ));
             result[i][j] = firstOperand + secondOperand;
         }
     }
@@ -63,7 +84,7 @@ double** duvdy(double** U, double **V, double dy, int nrl, int nrh, int ncl, int
 }
 
 
-double** d2udx2(double** U, double dx, int nrl, int nrh, int ncl, int nch)
+double** d2Mds2(double** M, double ds, int nrl, int nrh, int ncl, int nch)
 {
     int i = 0;
     int j = 0;
@@ -74,58 +95,77 @@ double** d2udx2(double** U, double dx, int nrl, int nrh, int ncl, int nch)
     {
         for( j = ncl; j < nch; ++j)
         {
-            result[i][j] = (U[i-1][j]-2*U[i][j]+U[i+1][j]) / pow(dx,2);
+            result[i][j] = (M[i-1][j] - 2*M[i][j] + M[i+1][j]) / pow(ds,2);
         }
      }
 
-
      return result;
+}
 
+/******** PRIVATE DERIVATIVE FUNCTIONS END ********/
+
+
+/******** DERIVATIVES FOR EQUATION (1) START ********/
+
+double** du2dx(double** U, double dx, int nrl, int nrh, int ncl, int nch)
+{
+   return dM2ds(U, dx, nrl, nrh, ncl, nch); 
+}
+
+double** duvdy(double** U, double **V, double dy, int nrl, int nrh, int ncl, int nch)
+{
+    return dMNds(U, V, dy, nrl, nrh, ncl, nch);
 }
 
 
+double** d2udx2(double** U, double dx, int nrl, int nrh, int ncl, int nch)
+{
+     return d2Mds2(U, dx, nrl, nrh, ncl, nch);
+}
 
 double** d2udy2(double** U, double dy, int nrl, int nrh, int ncl, int nch)
 {
-    int i = 0;
-    int j = 0;
-
-    double** result = NULL;
-
-    for( i = nrl; i < nrh; ++i)
-    {
-        for( j = ncl; j < nch; ++j)
-        {
-            result[i][j] = ( U[i-1][j]-2*U[i][j]+U[i+1][j]) / pow( dy,2 );
-        }
-    }
-
-    return result;
-
+    return d2Mds2(U, dy, nrl, nrh, ncl, nch);
 }
 
 double** dpdx(double** P, double dx, int nrl, int nrh, int ncl, int nch)
 {
-    int i = 0;
-    int j = 0;
-
-    double** result = NULL;
-
-    for( i = nrl; i < nrh; ++i)
-    {
-        for( j = ncl; j < nch; ++j)
-        {
-            result[i][j]=(P[i+1][j]-P[i][j])/dx;
-        }
-    }
-
-    return result;
-
+    return dMds(P, dx, nrl, nrh, ncl, nch);
 }
 
+/******** DERIVATIVES FOR EQUATION (1) END ********/
 
 
 
+/******** DERIVATIVES FOR EQUATION (2) START ********/
+
+
+double** dv2dy(double** V, double dy, int nrl, int nrh, int ncl, int nch)
+{
+    return dM2ds(V, dy, nrl, nrh, ncl, nch);
+}
+
+double** duvdx(double** U, double **V, double dx, int nrl, int nrh, int ncl, int nch)
+{
+    return dMNds(U, V, dx, nrl, nrh, ncl, nch);
+}
+
+double** d2vdx2(double** V, double dx, int nrl, int nrh, int ncl, int nch)
+{
+    return d2Mds2(V, dx, nrl, nrh, ncl, nch);
+}
+
+double** d2vdy2(double** V, double dy, int nrl, int nrh, int ncl, int nch)
+{
+    return d2Mds2(V, dy, nrl, nrh, ncl, nch);
+}
+
+double** dpdy(double** P, double dy, int nrl, int nrh, int ncl, int nch)
+{
+    return dMds(P, dy, nrl, nrh, ncl, nch);
+}
+
+/******** DERIVATIVES FOR EQUATION (2) END ********/
 
 
 
--- 01/MATERIAL/disc.h
+++ 01/MATERIAL/disc.h
@@ -1,12 +1,16 @@
 #ifndef __DISC_H__                                                                                    
 #define __DISC_H__
 
+
+/******** DERIVATIVES FOR EQUATION (1) ********/
 double** du2dx(double** U, double dx, int nrl, int nrh, int ncl, int nch);
 double** duvdy(double** U, double **V, double dy, int nrl, int nrh, int ncl, int nch);
 double** d2udx2(double** U, double dx, int nrl, int nrh, int ncl, int nch); 
 double** d2udy2(double** U, double dy, int nrl, int nrh, int ncl, int nch); 
 double** dpdx(double** P, double dx, int nrl, int nrh, int ncl, int nch);
 
+
+/******** DERIVATIVES FOR EQUATION (2) ********/
 double** dv2dy(double** V, double dy, int nrl, int nrh, int ncl, int nch);
 double** duvdx(double** U, double **V, double dx, int nrl, int nrh, int ncl, int nch);
 double** d2vdx2(double** V, double dx, int nrl, int nrh, int ncl, int nch); 
