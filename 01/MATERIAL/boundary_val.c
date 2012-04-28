cb7029cb5b935a5b3cb1aa3aa1657e1ddd272290
marco.seravalli@gmail.com <marco.seravalli@gmail.com>
2012-04-28 11:15:54 +0200

    init.c modified

---
+++ 01/MATERIAL/boundary_val.c
@@ -0,0 +1,15 @@
+#include "boundary_val.h"
+
+
+/**
+ * The boundary values of the problem are set.
+ */
+void boundaryvalues(
+  int imax,
+  int jmax,
+  double **U,
+  double **V
+)
+{
+    
+}
---
+++ 01/MATERIAL/uvp.c
@@ -0,0 +1,21 @@
+#include "uvp.h"
+
+
+void calculate_fg(
+  double Re,
+  double GX,
+  double GY,
+  double alpha,
+  double dt,
+  double dx,
+  double dy,
+  int imax,
+  int jmax,
+  double **U,
+  double **V,
+  double **F,
+  double **G
+)
+{
+
+}
