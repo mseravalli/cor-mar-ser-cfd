23ca732c3dce26b22b7071ee95dc0a73a33b3090
marco.seravalli@gmail.com <marco.seravalli@gmail.com>
2012-04-29 11:29:41 +0200

    added skeleton for computing G

--- 01/MATERIAL/uvp.c
+++ 01/MATERIAL/uvp.c
@@ -21,38 +21,101 @@ void calculate_fg(
   double **G
 )
 {
+    
+    /******** VARIABLE DECLARATION START ********/
+    
+    double** mult_Re;
+    double** mult_dt;
+
+    double** _d2udx2;
+    double** _d2udy2;
+    double** _du2dx ;
+    double** _duvdy ;
+
+    double** add_d2udx2_d2udy2;
+    double** sub_du2dx        ;
+    double** sub_duvdy        ;
+    double** add_GX           ;
+
+    double** _d2vdx2;
+    double** _d2vdy2;
+    double** _duvdx ;
+    double** _dv2dy ;
+
+    double** add_d2vdx2_d2vdy2;
+    double** sub_duvdx        ;
+    double** sub_dv2dy        ;
+    double** add_GY           ;
+
+    /******** VARIABLE DECLARATION END ********/
+
+
     /******** CALCULATE F START ********/
 
     /******** Calculate the single derivatives ********/
-    double** _d2udx2    = d2udx2(U, dx,     0, imax+1, 0, jmax+1);
-    double** _d2udy2    = d2udy2(U, dy,     0, imax+1, 0, jmax+1);
-    double** _du2dx     = du2dx(U, dx,      0, imax+1, 0, jmax+1);
-    double** _duvdy     = duvdy(U, V, dy,   0, imax+1, 0, jmax+1);
+    _d2udx2 = d2udx2(U, dx,   0, imax+1, 0, jmax+1);
+    _d2udy2 = d2udy2(U, dy,   0, imax+1, 0, jmax+1);
+    _du2dx  = du2dx(U, dx,    0, imax+1, 0, jmax+1);
+    _duvdy  = duvdy(U, V, dy, 0, imax+1, 0, jmax+1);
 
     /******** Calculate F ********/
-    double** add_d2udx2_d2udy2  = add_mat(_d2udx2, _d2udy2,         0, imax+1, 0, jmax+1);
-    double** mult_Re            = mult_scalar(add_d2udx2_d2udy2, Re,0, imax+1, 0, jmax+1);
-    double** sub_du2dx          = sub_mat(mult_Re, _du2dx,          0, imax+1, 0, jmax+1);
-    double** sub_duvdy          = sub_mat(sub_du2dx, _duvdy,        0, imax+1, 0, jmax+1);
-    double** add_GX             = add_scalar(sub_duvdy, GX,         0, imax+1, 0, jmax+1);
-    double** mult_dt            = mult_scalar(add_GX, dt,           0, imax+1, 0, jmax+1);
-    F                           = add_mat(U, mult_dt,               0, imax+1, 0, jmax+1);
+    add_d2udx2_d2udy2 = add_mat(_d2udx2, _d2udy2,            0, imax+1, 0, jmax+1);
+    mult_Re           = mult_scalar(add_d2udx2_d2udy2, 1/Re, 0, imax+1, 0, jmax+1);
+    sub_du2dx         = sub_mat(mult_Re, _du2dx,             0, imax+1, 0, jmax+1);
+    sub_duvdy         = sub_mat(sub_du2dx, _duvdy,           0, imax+1, 0, jmax+1);
+    add_GX            = add_scalar(sub_duvdy, GX,            0, imax+1, 0, jmax+1);
+    mult_dt           = mult_scalar(add_GX, dt,              0, imax+1, 0, jmax+1);
+    F                 = add_mat(U, mult_dt,                  0, imax+1, 0, jmax+1);
+
+    /******** Free the calulations ********/
+    free_matrix(mult_dt,           0, imax+1, 0, jmax+1);
+    free_matrix(add_GX,            0, imax+1, 0, jmax+1);
+    free_matrix(sub_duvdy,         0, imax+1, 0, jmax+1);
+    free_matrix(sub_du2dx,         0, imax+1, 0, jmax+1);
+    free_matrix(mult_Re,           0, imax+1, 0, jmax+1);
+    free_matrix(add_d2udx2_d2udy2, 0, imax+1, 0, jmax+1);
+
+    /******** Free the derivatives ********/
+    free_matrix(_duvdy,  0, imax+1, 0, jmax+1);
+    free_matrix(_du2dx,  0, imax+1, 0, jmax+1);
+    free_matrix(_d2udy2, 0, imax+1, 0, jmax+1);
+    free_matrix(_d2udx2, 0, imax+1, 0, jmax+1);
+
+    /******** CALCULATE F END ********/
+    
+
+    /******** CALCULATE G START ********/
+
+    /******** Calculate the single derivatives ********/
+    _d2vdx2 = d2vdx2(U, dx,   0, imax+1, 0, jmax+1);
+    _d2vdy2 = d2vdy2(U, dy,   0, imax+1, 0, jmax+1);
+    _duvdx  = duvdx(U, V, dy, 0, imax+1, 0, jmax+1);
+    _dv2dy  = dv2dy(U, dx,    0, imax+1, 0, jmax+1);
+
+    /******** Calculate G ********/
+    add_d2vdx2_d2vdy2 = add_mat(_d2vdx2, _d2vdy2,            0, imax+1, 0, jmax+1);
+    mult_Re           = mult_scalar(add_d2vdx2_d2vdy2, 1/Re, 0, imax+1, 0, jmax+1);
+    sub_duvdx         = sub_mat(mult_Re, _duvdx,             0, imax+1, 0, jmax+1);
+    sub_dv2dy         = sub_mat(sub_duvdx, _dv2dy,           0, imax+1, 0, jmax+1);
+    add_GY            = add_scalar(sub_dv2dy, GY,            0, imax+1, 0, jmax+1);
+    mult_dt           = mult_scalar(add_GY, dt,              0, imax+1, 0, jmax+1);
+    G                 = add_mat(V, mult_dt,                  0, imax+1, 0, jmax+1);
 
     /******** Free the calulations ********/
     free_matrix(mult_dt,            0, imax+1, 0, jmax+1);
-    free_matrix(add_GX,             0, imax+1, 0, jmax+1);
-    free_matrix(sub_duvdy,          0, imax+1, 0, jmax+1);
-    free_matrix(sub_du2dx,          0, imax+1, 0, jmax+1);
+    free_matrix(add_GY,             0, imax+1, 0, jmax+1);
+    free_matrix(sub_dv2dy,          0, imax+1, 0, jmax+1);
+    free_matrix(sub_duvdx,          0, imax+1, 0, jmax+1);
     free_matrix(mult_Re,            0, imax+1, 0, jmax+1);
-    free_matrix(add_d2udx2_d2udy2,  0, imax+1, 0, jmax+1);
+    free_matrix(add_d2vdx2_d2vdy2,  0, imax+1, 0, jmax+1);
 
     /******** Free the derivatives ********/
-    free_matrix(_duvdy,     0, imax+1, 0, jmax+1);
-    free_matrix(_du2dx,     0, imax+1, 0, jmax+1);
-    free_matrix(_d2udy2,    0, imax+1, 0, jmax+1);
-    free_matrix(_d2udx2,    0, imax+1, 0, jmax+1);
+    free_matrix(_dv2dy,     0, imax+1, 0, jmax+1);
+    free_matrix(_duvdx,     0, imax+1, 0, jmax+1);
+    free_matrix(_d2vdy2,    0, imax+1, 0, jmax+1);
+    free_matrix(_d2vdx2,    0, imax+1, 0, jmax+1);
 
-    /******** CALCULATE F END ********/
+    /******** CALCULATE G END ********/
 }
 
 void calculate_dt(
