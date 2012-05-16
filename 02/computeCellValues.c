2ae694d0531f21bf0766e094a8ede84f04c4c4f2
Milos Maric <maric@informatik.tu-muenchen.de>
2012-05-16 12:08:33 +0200

    functions in computecellValues implemented, not sure about correctnes

--- 02/computeCellValues.c
+++ 02/computeCellValues.c
@@ -1,14 +1,34 @@
 #include "computeCellValues.h"
+#include "LBDefinitions.h"
+#include "math.h"
 
 void computeDensity(const double *const currentCell, double *density){
-  /* TODO */
+    int i;
+    for(i = 0; i < Q; i++)
+    {
+        density += currentCell[i];
+    }
 }
 
 void computeVelocity(const double * const currentCell, const double * const density, double *velocity){
-  /* TODO */
+    int i;
+    for(i = 0; i<Q; i++)
+    {
+        velocity[1] += currentCell[i]*LATTICEVELOCITIES[i][1]/desity;
+        velocity[2] += currentCell[i]*LATTICEVELOCITIES[i][2]/desity;
+        velocity[3] += currentCell[i]*LATTICEVELOCITIES[i][3]/desity;
+    }
 }
 
 void computeFeq(const double * const density, const double * const velocity, double *feq){
- /* TODO */
+    int i;
+    double cs = sqrt(1/3);
+    double cispu; //scalar product of ci and u
+    double uspu = velocity[1]*velocity[1] + velocity[2]*velocity[2] + velocity[3]*velocity[3]; //scalar product of u and u
+    for(i = 0; i < Q; i++)
+    {
+        cispu = LATTICEVELOCITIES[i][1]*velocity[1] + LATTICEVELOCITIES[i][2]*velocity[2] + LATTICEVELOCITIES[i][3]*velocity[3];
+        feq[i] = LATTICEWEIGHTS[i] * density[i] * (1 + cispu/(cs*cs)) + (cispu*cispu)/(2*cs*cs*cs*cs) - uspu/(2*cs*cs);
+    }
 }
 
