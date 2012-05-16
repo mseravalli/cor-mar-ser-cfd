3ddbf454448250c365243f5e46af68303fe6b327
Milos Maric <maric@informatik.tu-muenchen.de>
2012-05-16 14:25:36 +0200

    functions in collision.c implemented

--- 02/collision.c
+++ 02/collision.c
@@ -1,10 +1,34 @@
 #include "collision.h"
+#include "computeCellValues.h"
+#include "LBDefinitions.h"
 
 void computePostCollisionDistributions(double *currentCell, const double * const tau, const double *const feq){
-  /* TODO */
+    int i;
+    for(i = 0; i < Q; i++)
+    {
+        currentCell[i] = currentCell[i] - 1/ (*tau) * (currentCell[i] - feq[i]);
+    }
 }
 
 void doCollision(double *collideField, int *flagField,const double * const tau,int xlength){
-  /* TODO */
+  int x, y, z;
+  double density, velocity[3], feq[Q];
+  
+  for(x = 0; x < xlength+2; x++)
+  {
+    for(y = 0; y < xlength+2; y++)
+    {
+        for(z = 0; z < xlength+2; z++)
+        {
+            if(flagField[z*xlength*xlength + y*xlength + x] == FLUID)
+            {
+                computeDensity(&collideField[Q*(z*xlength*xlength + y*xlength + x)], &density);
+                computeVelocity(&collideField[Q*(z*xlength*xlength + y*xlength + x)], &density, velocity);
+                computeFeq(&density, velocity, feq);
+                computePostCollisionDistributions(&collideField[Q*(z*xlength*xlength + y*xlength + x)], tau, feq);
+            }
+        }
+    }
+  }
 }
 
--- 02/computeCellValues.c
+++ 02/computeCellValues.c
@@ -6,7 +6,7 @@ void computeDensity(const double *const currentCell, double *density){
     int i;
     for(i = 0; i < Q; i++)
     {
-        density += currentCell[i];
+        *density += currentCell[i];
     }
 }
 
@@ -14,17 +14,17 @@ void computeVelocity(const double * const currentCell, const double * const dens
     int i;
     for(i = 0; i<Q; i++)
     {
-        velocity[1] += currentCell[i]*LATTICEVELOCITIES[i][1]/desity;
-        velocity[2] += currentCell[i]*LATTICEVELOCITIES[i][2]/desity;
-        velocity[3] += currentCell[i]*LATTICEVELOCITIES[i][3]/desity;
+        velocity[1] += currentCell[i]*LATTICEVELOCITIES[i][1]/(*density);
+        velocity[2] += currentCell[i]*LATTICEVELOCITIES[i][2]/(*density);
+        velocity[3] += currentCell[i]*LATTICEVELOCITIES[i][3]/(*density);
     }
 }
 
 void computeFeq(const double * const density, const double * const velocity, double *feq){
     int i;
     double cs = sqrt(1/3);
-    double cispu; //scalar product of ci and u
-    double uspu = velocity[1]*velocity[1] + velocity[2]*velocity[2] + velocity[3]*velocity[3]; //scalar product of u and u
+    double cispu; /* scalar product of ci and u */
+    double uspu = velocity[1]*velocity[1] + velocity[2]*velocity[2] + velocity[3]*velocity[3]; /* scalar product of u and u */
     for(i = 0; i < Q; i++)
     {
         cispu = LATTICEVELOCITIES[i][1]*velocity[1] + LATTICEVELOCITIES[i][2]*velocity[2] + LATTICEVELOCITIES[i][3]*velocity[3];
