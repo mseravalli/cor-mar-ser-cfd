8875202bb28242bf7c9dd0c26bb62a52c7720ae5
marco.seravalli@gmail.com <marco.seravalli@gmail.com>
2012-05-19 20:18:57 +0200

    final commit for WS2

--- 02/boundary.c
+++ 02/boundary.c
@@ -14,42 +14,107 @@ void treatBoundary(double *collideField, int* flagField, const double * const wa
     double cispu = 0;
     double wv = 0;
 
-    for (z = 0; z < xlength + 2; ++z) {
-        for (y = 0; y < xlength + 2; ++y) {
-            for (x = 0; x < xlength + 2; ++x) {
+
+    /* the 3 loops go only through the boundaries and skip the fluid */
+    for (x = 0; x < xlength + 2; x = x + xlength + 1) {
+        for (z = 0; z < xlength + 2; ++z) {
+            for (y = 0; y < xlength + 2; ++y) {
             
                 pos = ( z*(xlength+2)*(xlength+2) + y*(xlength+2) + x ); 
 
-                /* proceed only if we are at a boundary */
-                if (flagField[pos] == NO_SLIP || flagField[pos] == MOVING_WALL) {
+                for (i = 0; i < Q; ++i) {
 
-                    for (i = 0; i < Q; ++i) {
+                    /* check if the neighbour is within the domain */
+                    if (   (z+LATTICEVELOCITIES[i][2] > 0 && z+LATTICEVELOCITIES[i][2] < xlength + 1) 
+                        && (y+LATTICEVELOCITIES[i][1] > 0 && y+LATTICEVELOCITIES[i][1] < xlength + 1)
+                        && (x+LATTICEVELOCITIES[i][0] > 0 && x+LATTICEVELOCITIES[i][0] < xlength + 1) ) {
 
-                        /* check if the neighbour is within the domain */
-                        if (   (z+LATTICEVELOCITIES[i][2] > 0 && z+LATTICEVELOCITIES[i][2] < xlength + 1) 
-                            && (y+LATTICEVELOCITIES[i][1] > 0 && y+LATTICEVELOCITIES[i][1] < xlength + 1)
-                            && (x+LATTICEVELOCITIES[i][0] > 0 && x+LATTICEVELOCITIES[i][0] < xlength + 1) ) {
+                        nb = ( (z+LATTICEVELOCITIES[i][2])*(xlength+2)*(xlength+2) 
+                             + (y+LATTICEVELOCITIES[i][1])*(xlength+2) 
+                             + (x+LATTICEVELOCITIES[i][0]) );
 
-                            nb = ( (z+LATTICEVELOCITIES[i][2])*(xlength+2)*(xlength+2) 
-                                 + (y+LATTICEVELOCITIES[i][1])*(xlength+2) 
-                                 + (x+LATTICEVELOCITIES[i][0]) );
-
-                            wv = 0;
-                            if( flagField[pos] == MOVING_WALL) {
-                               computeDensity(&collideField[Q*nb],&density);
-                               cispu = LATTICEVELOCITIES[i][0]*wallVelocity[0] 
-                                     + LATTICEVELOCITIES[i][1]*wallVelocity[1] 
-                                     + LATTICEVELOCITIES[i][2]*wallVelocity[2];
-                               wv = 2*LATTICEWEIGHTS[i]*density*cispu/(C_S*C_S);
-                            }
-
-                            collideField[Q * pos + i] = collideField[Q * nb + (Q-1-i)] + wv;
+                        wv = 0;
+                        if( flagField[pos] == MOVING_WALL) {
+                           computeDensity(&collideField[Q*nb],&density);
+                           cispu = LATTICEVELOCITIES[i][0]*wallVelocity[0] 
+                                 + LATTICEVELOCITIES[i][1]*wallVelocity[1] 
+                                 + LATTICEVELOCITIES[i][2]*wallVelocity[2];
+                           wv = 2*LATTICEWEIGHTS[i]*density*cispu/(C_S*C_S);
                         }
+
+                        collideField[Q * pos + i] = collideField[Q * nb + (Q-1-i)] + wv;
                     }
                 }
             }
         }
     }
+
+    for (y = 0; y < xlength + 2; y = y + xlength + 1) {
+        for (z = 0; z < xlength + 2; ++z) {
+            for (x = 0; x < xlength + 2; ++x) {
+            
+                pos = ( z*(xlength+2)*(xlength+2) + y*(xlength+2) + x ); 
+
+                for (i = 0; i < Q; ++i) {
+
+                    /* check if the neighbour is within the domain */
+                    if (   (z+LATTICEVELOCITIES[i][2] > 0 && z+LATTICEVELOCITIES[i][2] < xlength + 1) 
+                        && (y+LATTICEVELOCITIES[i][1] > 0 && y+LATTICEVELOCITIES[i][1] < xlength + 1)
+                        && (x+LATTICEVELOCITIES[i][0] > 0 && x+LATTICEVELOCITIES[i][0] < xlength + 1) ) {
+
+                        nb = ( (z+LATTICEVELOCITIES[i][2])*(xlength+2)*(xlength+2) 
+                             + (y+LATTICEVELOCITIES[i][1])*(xlength+2) 
+                             + (x+LATTICEVELOCITIES[i][0]) );
+
+                        wv = 0;
+                        if( flagField[pos] == MOVING_WALL) {
+                           computeDensity(&collideField[Q*nb],&density);
+                           cispu = LATTICEVELOCITIES[i][0]*wallVelocity[0] 
+                                 + LATTICEVELOCITIES[i][1]*wallVelocity[1] 
+                                 + LATTICEVELOCITIES[i][2]*wallVelocity[2];
+                           wv = 2*LATTICEWEIGHTS[i]*density*cispu/(C_S*C_S);
+                        }
+
+                        collideField[Q * pos + i] = collideField[Q * nb + (Q-1-i)] + wv;
+                    }
+                }
+            }
+        }
+    }
+
+    for (z = 0; z < xlength + 2; z=z+xlength + 1) {
+        for (y = 0; y < xlength + 2; ++y) {
+            for (x = 0; x < xlength + 2; ++x) {
+            
+                pos = ( z*(xlength+2)*(xlength+2) + y*(xlength+2) + x ); 
+
+                for (i = 0; i < Q; ++i) {
+
+                    /* check if the neighbour is within the domain */
+                    if (   (z+LATTICEVELOCITIES[i][2] > 0 && z+LATTICEVELOCITIES[i][2] < xlength + 1) 
+                        && (y+LATTICEVELOCITIES[i][1] > 0 && y+LATTICEVELOCITIES[i][1] < xlength + 1)
+                        && (x+LATTICEVELOCITIES[i][0] > 0 && x+LATTICEVELOCITIES[i][0] < xlength + 1) ) {
+
+                        nb = ( (z+LATTICEVELOCITIES[i][2])*(xlength+2)*(xlength+2) 
+                             + (y+LATTICEVELOCITIES[i][1])*(xlength+2) 
+                             + (x+LATTICEVELOCITIES[i][0]) );
+
+                        wv = 0;
+                        if( flagField[pos] == MOVING_WALL) {
+                           computeDensity(&collideField[Q*nb],&density);
+                           cispu = LATTICEVELOCITIES[i][0]*wallVelocity[0] 
+                                 + LATTICEVELOCITIES[i][1]*wallVelocity[1] 
+                                 + LATTICEVELOCITIES[i][2]*wallVelocity[2];
+                           wv = 2*LATTICEWEIGHTS[i]*density*cispu/(C_S*C_S);
+                        }
+
+                        collideField[Q * pos + i] = collideField[Q * nb + (Q-1-i)] + wv;
+                    }
+                }
+            }
+        }
+    }
+
 }
 
 
--- 02/streaming.c
+++ 02/streaming.c
@@ -14,7 +14,6 @@ void doStreaming(double *collideField, double *streamField,int *flagField,int xl
         for (y = 0; y < xlength + 2; ++y) {
             for (x = 0; x < xlength + 2; ++x) {
                 pos = ( z*(xlength+2)*(xlength+2) + y*(xlength+2) + x ); 
-                /* from 0 to 19 */
                 
                 if (flagField[pos] == FLUID) {
                     for (i = 0; i < Q; ++i) {
@@ -22,9 +21,7 @@ void doStreaming(double *collideField, double *streamField,int *flagField,int xl
                             nb = ( (z+LATTICEVELOCITIES[i][2])*(xlength+2)*(xlength+2) 
                                  + (y+LATTICEVELOCITIES[i][1])*(xlength+2) 
                                  + (x+LATTICEVELOCITIES[i][0]) );
-                            /* probably not correct, need to check */
                             streamField[Q * pos + (Q-1-i)] = collideField[Q * nb + (Q-1-i)];
-
 
                     }
                 }
