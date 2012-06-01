b0451f0d6d3f797985b478052b1cb782fa076208
flaviocordero <flaviocordero@gmail.com>
2012-06-01 17:42:39 +0000

    uv_comm implemented, not sure if it is working

--- 04/parallel.c
+++ 04/parallel.c
@@ -171,5 +171,149 @@ void uv_comm(double **U,
               MPI Status *status,
               int chunk)
 {
+    int imax = ir - il + 1;
+    int jmax = jt - jb + 1;
+    int i = 0;
+    int j = 0;
+
+    /* Velocity U*/
+
+    /* Send-Recv Left-Right */
+
+    if (rank_l != MPI_PROC_NULL){
+        for (j= 1;j <= jmax;j++)
+        {
+            bufSend[j-1] = U[2][j];
+        }
+    }
+
+        MPI_Sendrecv(bufSend, jmax, MPI_DOUBLE, rank_l, chunk, bufRecv, jmax, MPI_DOUBLE, rank_r, chunk, MPI_COMM_WORLD, status);
+
+    if (rank_r !=MPI_PROC_NULL){
+        for (j= 1;j <= jmax;j++){
+             U[imax+1][j]=bufRecv[j-1];
+        }
+    }
+
+     /* Send-Recv Right-Left */
+
+    if (rank_r != MPI_PROC_NULL){
+        for (j= 1;j <= jmax;j++)
+        {
+            bufSend[j-1] = U[imax-1][j];
+        }
+    }
+
+        MPI_Sendrecv(bufSend, jmax, MPI_DOUBLE, rank_r, chunk, bufRecv, jmax, MPI_DOUBLE, rank_l, chunk, MPI_COMM_WORLD, status);
+
+    if (rank_r !=MPI_PROC_NULL){
+        for (j= 1;j <= jmax;j++){
+             U[0][j]=bufRecv[j-1];
+        }
+    }
+
+     /* Send-Recv Top-Bot */
+
+    if (rank_t != MPI_PROC_NULL){
+        for (i= 1; i <= imax; i++)
+        {
+            bufSend[i-1] = U[i][jmax];
+        }
+    }
+       MPI_Sendrecv(bufSend, jmax, MPI_DOUBLE, rank_t, chunk, bufRecv, jmax, MPI_DOUBLE, rank_b, chunk, MPI_COMM_WORLD, status);
+
+    if (rank_b !=MPI_PROC_NULL){
+        for (i= 1;i <= imax;i++){
+             U[i][0]=bufRecv[i-1];
+        }
+    }
+
+    /* Send-Recv Bot-Top */
+
+    if (rank_b != MPI_PROC_NULL){
+        for (i= 1; i <= imax; i++)
+        {
+            bufSend[i-1] = U[i][1];
+        }
+    }
+
+        MPI_Sendrecv(bufSend, jmax, MPI_DOUBLE, rank_b, chunk, bufRecv, jmax, MPI_DOUBLE, rank_t, chunk, MPI_COMM_WORLD, status);
+
+    if (rank_t !=MPI_PROC_NULL){
+        for (i= 1;i <= imax;i++){
+             U[i][jmax+1]=bufRecv[i-1];
+        }
+    }
+
+
+     /* Velocity V */
+
+    /* Send-Recv Left-Right */
+
+    if (rank_l != MPI_PROC_NULL){
+        for (j= 0; j <= jmax + 1 ; j++)
+        {
+            bufSend[j] = V[1][j];
+        }
+    }
+
+        MPI_Sendrecv(bufSend, jmax, MPI_DOUBLE, rank_l, chunk, bufRecv, jmax, MPI_DOUBLE, rank_r, chunk, MPI_COMM_WORLD, status);
+
+    if (rank_r !=MPI_PROC_NULL){
+        for (j= 0; j <= jmax + 1; j++){
+             V[imax+1][j]=bufRecv[j];
+        }
+    }
+    
+     /* Send-Recv Right-Left */
+
+    if (rank_r != MPI_PROC_NULL){
+        for (j= 0; j <= jmax + 1; j++)
+        {
+            bufSend[j] = V[imax][j];
+        }
+    }
+
+        MPI_Sendrecv(bufSend, jmax, MPI_DOUBLE, rank_r, chunk, bufRecv, jmax, MPI_DOUBLE, rank_l, chunk, MPI_COMM_WORLD, status);
+
+    if (rank_r !=MPI_PROC_NULL){
+        for (j = 0 ; j <= jmax +1; j++){
+             V[0][j] = bufRecv[j];
+        }
+    }
+
+     /* Send-Recv Top-Bot */
+
+    if (rank_t != MPI_PROC_NULL){
+        for (i = 1; i <= imax; i++)
+        {
+            bufSend[i-1] = V[i][jmax];
+        }
+    }
+
+        MPI_Sendrecv(bufSend, jmax, MPI_DOUBLE, rank_t, chunk, bufRecv, jmax, MPI_DOUBLE, rank_b, chunk, MPI_COMM_WORLD, status);
+
+    if (rank_b != MPI_PROC_NULL){
+        for (i= 1;i <= imax;i++){
+             V[i][0] = bufRecv[i-1];
+        }
+    }
+
+    /* Send-Recv Bot-Top */
+
+    if (rank_b != MPI_PROC_NULL){
+        for (i= 1; i <= imax; i++)
+        {
+            bufSend[i-1] = V[i][1];
+        }
+    }
+
+        MPI_Sendrecv(bufSend, jmax, MPI_DOUBLE, rank_b, chunk, bufRecv, jmax, MPI_DOUBLE, rank_t, chunk, MPI_COMM_WORLD, status);
+
+    if (rank_t !=MPI_PROC_NULL){
+        for (i= 1;i <= imax;i++){
+             V[i][jmax+1] = bufRecv[i-1];
+        }
+    }
 
 }
